3db07d73dc32f14bab776d4fdcc9bad55293e68a
mohamed44g <bdry5647@gmail.com>
2025-06-09 21:34:39 +0300

    update Dashboard page and make the components loaded lazly and make the font responsive based on veiwport

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { ThemeProvider, CssBaseline} from "@mui/material";
+import { ThemeProvider, CssBaseline, responsiveFontSizes } from "@mui/material";
 import Dashboard from "./pages/dashboard";
 import { theme } from "./theme/theme";
 
 function App() {
+  const App_theme = responsiveFontSizes(theme);
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={App_theme}>
       <CssBaseline />
       <Dashboard />
     </ThemeProvider>
